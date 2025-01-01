9de9796a5972d8c81b769ec243c23f0448305e99
John Dave Decano <johndavedecano@gmail.com>
2025-01-02 00:30:36 +0100

    add layout and sidebar components for improved navigation structure

--- assets/app.ts
+++ assets/app.ts
@@ -1,5 +1,6 @@
 import 'htmx.org';
 import './app.css';
 import './custom.css';
+import './layout.css';
 
 console.log('Hello Vite + ActixWeb(Rust) + TypeScript + Tailwind CSS + HTMX!');
--- src/routes/public.rs
+++ src/routes/public.rs
@@ -1,18 +1,28 @@
 use actix_web::{web, Responder, Result};
 use askama::Template;
 
+
 #[derive(Template)]
 #[template(path = "index.html")]
 struct Index;
-
-#[derive(Template)]
-#[template(path = "404.html")]
-struct NotFound;
 
 async fn index() -> Result<impl Responder> {
     let html = Index.render().expect("Failed to render index.html");
     Ok(web::Html::new(html))
 }
+
+#[derive(Template)]
+#[template(path = "dashboard.html")]
+struct Dashboard;
+
+async fn dashboard() -> Result<impl Responder> {
+    let html = Dashboard.render().expect("Failed to render dashboard.html");
+    Ok(web::Html::new(html))
+}
+
+#[derive(Template)]
+#[template(path = "404.html")]
+struct NotFound;
 
 async fn not_found() -> Result<impl Responder> {
     let html = NotFound.render().expect("Failed to render 404.html");
@@ -22,5 +32,6 @@ async fn not_found() -> Result<impl Responder> {
 // Function to register routes
 pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg.route("/", web::get().to(index));
+    cfg.route("/dashboard", web::get().to(dashboard));
     cfg.default_service(web::to(not_found));
 }
