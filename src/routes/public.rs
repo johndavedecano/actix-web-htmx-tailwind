48435500013c73728dcab3d8a6442328baaeb14e
John Dave Decano <johndavedecano@gmail.com>
2025-01-02 00:53:47 +0100

    add forgot and reset password pages with routing updates

--- src/routes/public.rs
+++ src/routes/public.rs
@@ -21,6 +21,43 @@ async fn dashboard() -> Result<impl Responder> {
 }
 
 #[derive(Template)]
+#[template(path = "forgot.html")]
+struct Forgot;
+
+async fn forgot() -> Result<impl Responder> {
+    let html = Forgot.render().expect("Failed to render forgot.html");
+    Ok(web::Html::new(html))
+}
+
+#[derive(Template)]
+#[template(path = "login.html")]
+struct Login;
+
+async fn login() -> Result<impl Responder> {
+    let html = Login.render().expect("Failed to render login.html");
+    Ok(web::Html::new(html))
+}
+
+#[derive(Template)]
+#[template(path = "register.html")]
+struct Register;
+
+async fn register() -> Result<impl Responder> {
+    let html = Register.render().expect("Failed to render register.html");
+    Ok(web::Html::new(html))
+}
+
+#[derive(Template)]
+#[template(path = "reset.html")]
+struct Reset;
+
+async fn reset() -> Result<impl Responder> {
+    let html = Reset.render().expect("Failed to render reset.html");
+    Ok(web::Html::new(html))
+}
+
+
+#[derive(Template)]
 #[template(path = "404.html")]
 struct NotFound;
 
@@ -33,5 +70,9 @@ async fn not_found() -> Result<impl Responder> {
 pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg.route("/", web::get().to(index));
     cfg.route("/dashboard", web::get().to(dashboard));
+    cfg.route("/login", web::get().to(login));
+    cfg.route("/register", web::get().to(register));
+    cfg.route("/forgot", web::get().to(forgot));
+    cfg.route("/reset", web::get().to(reset));
     cfg.default_service(web::to(not_found));
 }
