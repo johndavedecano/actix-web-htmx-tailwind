b711cb52f91e52c1d643626d2a8b9e1e943f1a51
John Dave Decano <johndavedecano@gmail.com>
2025-01-01 23:40:59 +0100

    add 404 error page and update routing to handle not found errors

--- src/routes/public.rs
+++ src/routes/public.rs
@@ -5,12 +5,22 @@ use askama::Template;
 #[template(path = "index.html")]
 struct Index;
 
+#[derive(Template)]
+#[template(path = "404.html")]
+struct NotFound;
+
 async fn index() -> Result<impl Responder> {
-    let html = Index.render().expect("template should be valid");
+    let html = Index.render().expect("Failed to render index.html");
+    Ok(web::Html::new(html))
+}
+
+async fn not_found() -> Result<impl Responder> {
+    let html = NotFound.render().expect("Failed to render 404.html");
     Ok(web::Html::new(html))
 }
 
 // Function to register routes
 pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg.route("/", web::get().to(index));
+    cfg.default_service(web::to(not_found));
 }
